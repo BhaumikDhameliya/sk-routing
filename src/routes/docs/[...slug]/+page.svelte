bef1d2910abbc2682fb85e2d12462f5d236b4c48
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-02-11 21:21:34 +0530

    catch all routes

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+	const slug = $page.params.slug;
+	const slugArr = slug.split('/');
+</script>
+
+<h1>Docs home page</h1>
+{#if slugArr.length === 1}
+	<h1>Viewing docs for feature {slugArr[0]}</h1>
+{:else if slugArr.length === 2}
+	<h1>Viewing docs for feature {slugArr[0]} and concept {slugArr[1]}</h1>
+{/if}
