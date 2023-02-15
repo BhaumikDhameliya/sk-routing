f6a73459c7bf9f100123d4dfebe1f7af5e38b550
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-02-15 16:28:33 +0530

    Route Layout

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -5,13 +5,13 @@
 <a href="/">Home</a>
 
 <h1>Product List</h1>
-<a href="/products/product1">
+<a href="/products/1">
 	<h2>Product 1</h2>
 </a>
-<a href="/products/product2">
+<a href="/products/2">
 	<h2>Product 2</h2>
 </a>
-<a href="/products/product3">
+<a href="/products/3">
 	<h2>Product 3</h2>
 </a>
 <a href="/products/{productId}">
