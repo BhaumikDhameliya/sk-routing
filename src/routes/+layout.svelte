2c76dcff895d3fa606ff119205fbf6fea032754c
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-02-18 21:19:02 +0530

    Other Files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,8 @@
 <script>
+	import Header from '$lib/header.svelte';
+	import Footer from '$lib/footer.svelte';
 </script>
 
-<header class="layout-header">Header</header>
+<Header />
 <slot />
-<footer class="layout-footer">Footer</footer>
-
-<style>
-	.layout-header {
-		background-color: #264653;
-		color: #f4a261;
-		font-size: 30px;
-		text-align: center;
-		padding: 10px;
-	}
-	.layout-footer {
-		background-color: #264653;
-		color: #f4a261;
-		font-size: 30px;
-		text-align: center;
-		padding: 10px;
-	}
-</style>
+<Footer />
