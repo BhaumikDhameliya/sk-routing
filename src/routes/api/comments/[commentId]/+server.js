e731070360c1443e0d7a5e232b4496016c7b2c3a
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-02-20 22:24:41 +0530

    PATCH and DELETE api

---
+++ src/routes/api/comments/[commentId]/+server.js
@@ -0,0 +1,38 @@
+import { comments } from '$lib/comments';
+import { json } from '@sveltejs/kit';
+
+export function GET(requestEvent) {
+	const { params } = requestEvent;
+	const { commentId } = params;
+	const comment = comments.find((comment) => comment.id === parseInt(commentId));
+
+	return json(comment);
+}
+
+export async function PATCH(requestEvent) {
+	const { params, request } = requestEvent;
+	const { commentId } = params;
+	const updatedComment = await request.json();
+	let cmnt;
+	comments.forEach((comment, index) => {
+		if (comment.id === parseInt(commentId)) {
+			cmnt = { ...comments[index], ...updatedComment };
+			comments[index] = cmnt;
+		}
+	});
+	if (!cmnt) {
+		return json({ message: 'Comment not found' }, { status: 404 });
+	}
+	return json(cmnt);
+}
+
+export async function DELETE(requestEvent) {
+	const { params } = requestEvent;
+	const { commentId } = params;
+	const commentIndex = comments.findIndex((comment) => comment.id === parseInt(commentId));
+	if (commentIndex === -1) {
+		return json({ message: 'Comment not found' }, { status: 404 });
+	}
+	comments.splice(commentIndex, 1);
+	return json({ message: 'comment removed.' });
+}
