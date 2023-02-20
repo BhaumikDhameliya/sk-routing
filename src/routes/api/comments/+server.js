71dee390a24414c4a77ced4a507e508aebb8781a
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-02-20 11:23:52 +0530

    API Get Request

---
+++ src/routes/api/comments/+server.js
@@ -0,0 +1,12 @@
+import { comments } from '$lib/comments';
+import { json } from '@sveltejs/kit';
+
+export function GET() {
+	// return new Response(JSON.stringify(comments), {
+	// 	headers: {
+	// 		'Content-Type': 'application/json'
+	// 	}
+	// });
+
+	return json(comments);
+}
